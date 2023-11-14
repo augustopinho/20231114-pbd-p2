1b930a54590b010b3dd1f9ef78f53090d2d17c00
Augusto Pinho <augustopinho@outlook.com>
2023-11-14 08:43:24 -0300

    feat(script_SQL): criando tabela base

--- 20231114-pbd-p2.sql
+++ 20231114-pbd-p2.sql
@@ -1,1 +1,9 @@
---Atividade Avaliativa (p2):
+/*Atividade Avaliativa (p2):*/
+CREATE TABLE tb_youtubers (
+	rank INT PRIMARY KEY,
+	youtuber VARCHAR(60),
+	subscribers INT,
+	video_count INT,
+	category VARCHAR (40),
+	year_started INT
+);
