30a64506e673143bb76035a7fc3e65d0a5cc2d2a
Augusto Pinho <augustopinho@outlook.com>
2023-11-14 09:02:30 -0300

    feat(script_SQL): Tabela pronta e importando dados

--- 20231114-pbd-p2.sql
+++ 20231114-pbd-p2.sql
@@ -1,9 +1,12 @@
 /*Atividade Avaliativa (p2):*/
+DROP TABLE tb_youtubers CASCADE;
 CREATE TABLE tb_youtubers (
 	rank INT PRIMARY KEY,
 	youtuber VARCHAR(60),
-	subscribers INT,
-	video_count INT,
+	subscribers BIGINT,
+	video_view BIGINT,
+	video_count BIGINT,
 	category VARCHAR (40),
 	year_started INT
 );
+
